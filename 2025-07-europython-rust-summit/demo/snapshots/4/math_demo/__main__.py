34fb68e4708de813cb064287541d6a27a6463747
Julian Hofer <julianhofer@gnome.org>
2025-07-08 17:18:51 +0200

    Small improvements

--- 2025-07-europython-rust-summit/demo/snapshots/4/math_demo/__main__.py
+++ 2025-07-europython-rust-summit/demo/snapshots/4/math_demo/__main__.py
@@ -1,5 +1,6 @@
 from cowpy.cow import Cowacter
+
 from .math_demo import add  # type: ignore
 
-message = Cowacter().milk(f"3 + 5 = {add(3, 5)}")
+message = Cowacter().milk("3 + 5 = " + str(add(3, 5)))
 print(message)
